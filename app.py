049116eb8e50c80a0a8521427006094eabadb148
SUJIT SHIBAPRASAD MAITY <sujitmaity.coder@gmail.com>
2024-08-04 02:28:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,11 @@ class Board:
         return pd.DataFrame(visible_board)
 
 def main():
+    # Set page configuration
+    st.set_page_config(page_title="Minesweeper",
+                       layout="wide",
+                       page_icon="👾")
+    
     st.title("Minesweeper")
 
     # Sidebar for settings
